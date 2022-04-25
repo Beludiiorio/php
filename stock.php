fe98d7cc502e1f94c308f2567ef4d391e3e1aa52
Beludiiorio <beludiiorio@gmail.com>
2022-04-25 11:06:01 -0300

    cambios

--- stock.php
+++ stock.php
@@ -7,3 +7,25 @@ if ($stock > 0) {
 } else { echo "No hay stock";
 }
 ?>
+
+<br>
+
+<!---Otro ejercicio-->
+<?php 
+$valor=rand(100,300); 
+print_r($valor)
+?>
+
+<br>
+
+<!---Otro ejercicio-->
+
+<?php 
+
+$valor= rand(1,5);
+
+    if ( $valor== 1|| $valor == 3 || $valor== 5) {
+    echo "El numero $valor es impar" ;
+}
+    else { echo "El numero $valor es par"; }
+?>
