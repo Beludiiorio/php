afb3c4dce3c474be5784ee6657154135e66eb8f6
Beludiiorio <beludiiorio@gmail.com>
2022-04-29 10:33:38 -0300

    cambios

--- funcion_promediar.php
+++ funcion_promediar.php
@@ -17,5 +17,6 @@ function promediar($aNumeros){
 $aNotas = array(8, 4, 5, 3, 9, 1);
 $aSueldo = array(800.30, 400.87, 500.45, 300, 900.70, 100, 900, 1800);
 
-echo "La nota promedio es: " . promediar($aNotas) . "<br>" ;
+echo "La nota promedio es: " . promediar($aNotas) ;
  
+?>
