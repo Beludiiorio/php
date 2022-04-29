4ea95873fc15c15c582c20acecfce9bcef8e4323
Beludiiorio <beludiiorio@gmail.com>
2022-04-29 10:00:51 -0300

    cambios

--- funcion_promediar.php
+++ funcion_promediar.php
@@ -4,26 +4,18 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 function promediar($aNumeros){
-    $sumatoria = 0;
+    $suma = 0;
     foreach ($aNumeros as $numero) {
-        $sumatoria = $sumatoria + $numero;
+        $suma = $suma + $numero;
     }
-    return $sumatoria / count($aNumeros);
+    $promedio = $suma / count($aNumeros);
+    return $promedio;
 }
 
-function maximo($aVector){
-    $maximo = 0;
-    foreach($aVector as $item){
-        if($maximo < $item){
-            $maximo = $item;
-        }
-    }
-    return $maximo;
-}
 
 //Uso
 $aNotas = array(8, 4, 5, 3, 9, 1);
 $aSueldo = array(800.30, 400.87, 500.45, 300, 900.70, 100, 900, 1800);
 
-echo "La nota promedio es: " . promediar($aNotas) . "<br>" . "<br>";
-echo "El sueldo máximo es: " . maximo($aSueldo); 
+echo "La nota promedio es: " . promediar($aNotas) . "<br>" ;
+ 
