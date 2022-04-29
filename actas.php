ee75b2ea2e03b923f303877ac9cca846a5ab1cff
Beludiiorio <beludiiorio@gmail.com>
2022-04-29 13:30:12 -0300

    cambios

--- actas.php
+++ actas.php
@@ -1,24 +1,19 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-//Definición de array de alumnos:
 
 $aAlumnos = array();
-$aAlumnos[] = array("nombre" => "Juan Perez", "aNotas" => array(9, 8));
-$aAlumnos[] = array("nombre" => "Ana Valle", "aNotas" => array(4, 9));
-$aAlumnos[] = array("nombre" => "Gonzalo Roldán", "aNotas" => array(7, 6));
-$aAlumnos[] = array("nombre" => "Maria Gonzalez", "aNotas" => array(9, 7));
-$aAlumnos[] = array("nombre" => "Claudia Dumycz", "aNotas" => array(10, 9));
+$aAlumnos[] = array("nombre" => "Ana Valle", "notas" => array(7, 8));
+$aAlumnos[] = array("nombre" => "Bernabe Paz", "notas" => array(5, 7));
+$aAlumnos[] = array("nombre" => "Sebastian Aguirre", "notas" => array(6, 9));
+$aAlumnos[] = array("nombre" => "Juana Martinez", "notas" => array(8, 9));
+$aAlumnos[] = array("nombre" => "Claudia Dumycz", "notas" => array(10, 9));
 
-function promediar($aNumeros){
-    $suma = 0;
-    $resultado = 0;
-    foreach($aNumeros as $numero){
-        $suma += $numero;
+function promediar($aNumeros)
+{
+    $sumatoria = 0;
+    foreach ($aNumeros as $numero) {
+        $sumatoria = $sumatoria + $numero;
     }
-    return $suma / count($aNumeros);
+    return $sumatoria / count($aNumeros);
 }
 
 ?>
@@ -30,53 +25,51 @@ function promediar($aNumeros){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <main class="container">
         <div class="row">
-            <div class="col-12 my-3 text-center">
-                <h1>Actas</h1>
-            </div>
-        </div>
-        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="py-5">Actas</h1>
+            </div>            
             <div class="col-12">
                 <table class="table table-dark table-hover border">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Alumno</th>
-                            <th>Nota 1</th>
-                            <th>Nota 2</th>
-                            <th>Promedio</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                    <tr>
+                        <th>Alumno</th>
+                        <th>Nota 1</th>
+                        <th>Nota 2</th>
+                        <th>Promedio</th>
+                    </tr>
                     <?php 
-                    $pos = 0;
-                    $sumPromedios = 0;
+                    $promedioCursada = 0;
+                    $sumatoria = 0;
+
                     foreach ($aAlumnos as $alumno): 
-                        $pos++;
-                        $promedio = promediar($alumno["aNotas"]);
-                        $sumPromedios += $promedio;
+                        $promedio = promediar($alumno["notas"]);
+                        $sumatoria = $sumatoria + $promedio;
                     ?>
                         <tr>
-                            <td><?php echo $pos; ?></td>
                             <td><?php echo $alumno["nombre"]; ?></td>
-                            <td><?php echo $alumno["aNotas"][0]; ?></td>
-                            <td><?php echo $alumno["aNotas"][1]; ?></td>
+                            <td><?php echo $alumno["notas"][0]; ?></td>
+                            <td><?php echo $alumno["notas"][1]; ?></td>
                             <td><?php echo number_format($promedio, 2, ",", "."); ?></td>
                         </tr>
-                        <?php endforeach; ?>
-                    </tbody>
+                    <?php 
+                         
+                    endforeach; 
+                    $promedioCursada = $sumatoria / count($aAlumnos);
+                    ?>
                 </table>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12 my-3">
-                <h4>Promedio de la cursada: <?php echo number_format($sumPromedios / count($aAlumnos), 2, ",", "."); ?></h4>
+            <div class="row">
+                <div class="col-12">
+                    <p>Promedio de la cursada: <?php echo number_format($promedioCursada, 2, ",", "."); ?> </p>
+                </div>
             </div>
+
         </div>
     </main>
+    
 </body>
 </html>
