186ec06189c41751beb5d68023a0c9e5251dfdd7
Beludiiorio <beludiiorio@gmail.com>
2022-05-18 15:45:49 -0300

    cambios

--- contar.php
+++ contar.php
@@ -3,26 +3,52 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-#Definición
-$aNotas = array(9, 8, 9.50, 4, 7, 8);
+$aPacientes = array();
+$aPacientes[] = array(
+    "dni" => "33.765.012",
+    "nombre" => "Ana Acuña",
+    "edad" => 45,
+    "peso" => 82
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Gonzalo Bustamante",
+    "edad" => 66,
+    "peso" => 79,
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Juan Irraola",
+    "edad" => 28,
+    "peso" => 90,
+);
+$aPacientes[] = array(
+    "dni" => "36.630.478",
+    "nombre" => "Beatriz Ocampo",
+    "edad" => 50,
+    "peso" => 83,
+
+);
+
+$aNotas=  array(9,8,9.50,4,7,8);
+$aEmpleados = array("gomez","ramirez","di iorio","barrionuevo","gonzalez","perez","tucci");
 
 
-$aEmpleados = array();
-$aEmpleados[] = array("dni" => 33300123, "nombre" => "David García", "bruto" => 85000.30);
-$aEmpleados[] = array("dni" => 40874456, "nombre" => "Ana Del Valle", "bruto" => 90000);
-$aEmpleados[] = array("dni" => 67567565, "nombre" => "Andrés Perez", "bruto" => 90000);
-$aEmpleados[] = array("dni" => 75744545, "nombre" => "Victoria Luz", "bruto" => 90000);
 
-
-function contar($aArray){
-    $cont = 0;
-    foreach($aArray as $aArray){
-        $cont++;
+function contar ($aArray){
+    $contador = 0;
+    foreach ($aArray as $item) {
+         $contador++;
     }
-    return $cont;
+    return $contador;
+
+     
+
 }
 
-#Uso:
-echo "Cantidad de notas: " . contar($aNotas);
+echo "cantidad de pacientes es: ". contar($aPacientes) . "<br>";
+echo "cantidad de empleados es: ". contar($aEmpleados) . "<br>";
+echo "cantidad de notas es: ". contar($aNotas);
+
 
 ?>
