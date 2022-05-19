ce1e7b252be4701bcb1a6db3225306ed9408cc30
Beludiiorio <beludiiorio@gmail.com>
2022-05-19 19:36:35 -0300

    cambios

--- formulario_login.php/index.php
+++ formulario_login.php/index.php
@@ -3,56 +3,69 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if($_POST){ /* es postback ? */
 
-    $usuario = $_REQUEST["txtUsuario"];
-    $clave = $_REQUEST["txtClave"];
+//si es postback
+if ($_POST) {
+    $usuario = $_POST["txtUsuario"];
+    $clave = $_POST["txtClave"];
 
-    //Si usuario es distinto de vacio Y clave es distinto de vacio, entonces:
-    if($usuario != "" && $clave != ""){
-        header("Location: acceso-confirmado.php");
+
+    //Si nombre es distinto de vacio y clave es distinto de vacio entonces:
+    if ($usuario != "" &&  $clave != "") {
+        //redireccionar a acceso-confirmado.php
+        header("Location:acceso-confirmado.php");
+        //sino 
     } else {
         $mensaje = "Válido para usuarios registrados.";
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
+    <title>Formulario </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
 </head>
+
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 mt-5">
-                <h1>Formulario: </h1>
+    <main class="container">
+        <div class="row" >
+            <div class="col-12 text-center py-3 ">
+                <h1> Formulario </h1>
             </div>
         </div>
+
         <div class="row">
-            <div class="col-12"> 
-                <?php if (isset($mensaje)): ?>
+            <div class="col-12">
+                <!--isset lo usamos para saber si existe la variable mensaje -->
+                <?php if (isset($mensaje)) { ?>
                     <div class="alert alert-danger" role="alert">
                         <?php echo $mensaje; ?>
                     </div>
-                <?php endif; ?>
+                <?php } ?>
                 <form method="POST" action="">
-                    <div class="my-3">
-                        <label for="">Usuario: <input type="text" id="txtUsuario" name="txtUsuario" class="form-control"></label>
+                    <div>
+                        <label for="txtUsuario">Usuario:</label>
+                            <input type="text" id="txtUsuario" name="txtUsuario" class="form-control">
                     </div>
-                    <div class="my-3">
-                        <label for="">Clave: <input type="password" id="txtClave" name="txtClave" class="form-control"></label>
+                    <div class="py-3">
+                        <label for="txtClave">Clave:</label>
+                            <input type="password" id="txtClave" name="txtClave" class="form-control">
                     </div>
-                    <div class="my-3">
+                    <div class="py-3">
                         <button class="btn btn-primary" type="submit">ENVIAR</button>
                     </div>
                 </form>
             </div>
         </div>
 
-    </div>
+    </main>
 </body>
+
 </html>
