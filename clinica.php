dbebdf88b6ebf92dd47d104057bd147ef27ba34f
Beludiiorio <beludiiorio@gmail.com>
2022-05-29 14:26:28 -0300

    cambios

--- clinica.php
+++ clinica.php
@@ -1,6 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-//Definicion de pacientes 
 $aPacientes = array();
 $aPacientes[] = array(
     "dni" => "33.765.012",
@@ -8,67 +10,75 @@ $aPacientes[] = array(
     "edad" => 45,
     "peso" => 81.50,
 );
-$aPacientes[] = array(
+
+$aPacientes[2] = array(
     "dni" => "23.684.385",
     "nombre" => "Gonzalo Bustamante",
     "edad" => 66,
-    "peso" => 79,
+    "peso" => 79
 );
-$aPacientes[] = array(
-    "dni" => "38.114.385",
-    "nombre" => "Martín Irraola",
+$aPacientes[3] = array(
+    "dni" => "23.684.386",
+    "nombre" => "Juan Iraola",
     "edad" => 28,
-    "peso" => 79,
+    "peso" => 70
 );
-$aPacientes[] = array(
-    "dni" => "29.684.385",
+$aPacientes[4] = array(
+    "dni" => "23.684.792",
     "nombre" => "Beatriz Ocampo",
     "edad" => 50,
-    "peso" => 79,
+    "peso" => 79
 );
 
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clinica</title>
+    <title> Listado de pacientes </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+
 <body>
-    <div class="container">
-    <div class="row">
-        <div class="col-12 py-3 text-center">
-            <h1>Listado de pacientes</h1>
+    <main>
+        <div class="container">
+            <div class="col-12 text-center py-3">
+                <h1> Listado de pacientes </h1>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <table class="table table-dark table-hover border">
-                <tr>
-                    <th>DNI</th>
-                    <th>Nombre y apellido</th>
-                    <th>Edad</th>
-                    <th>Peso</th>
-                </tr>
-                <?php foreach ($aPacientes as $paciente) { //A partir de aca comienza el bucle
-           
-           
-            ?>
-                <tr>
-                    <td><?php echo $paciente["dni"];  ?></td>
-                    <td><?php echo $paciente["nombre"];  ?></td>
-                    <td><?php echo $paciente["edad"];  ?></td>
-                    <td><?php echo $paciente["peso"];  ?></td>
-                </tr>
-                <?php } // Aca termina el bucle 
-                ?>
-            </table>
+
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <tr>
+                        <th> DNI</th>
+                        <th>Nombre y apellido </th>
+                        <th> Edad </th>
+                        <th> Peso </th>
+                    </tr>
+
+                    <?php foreach ($aPacientes as $pos => $paciente) { //Empieza el bucle 
+
+
+                    ?>
+                        <tr>
+                            <td><?php echo $paciente["dni"];  ?></td>
+                            <td> <?php echo $paciente["nombre"]; ?></td>
+                            <td> <?php echo $paciente["edad"]; ?></td>
+                            <td> <?php echo $paciente["peso"]; ?></td>
+                        </tr>
+
+                    <?php } //Termina el bucle  
+                    ?>
+
+                </table>
+            </div>
         </div>
-    </div>
-    </div>
+    </main>
 </body>
+
 </html>
