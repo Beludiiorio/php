c03dc703998cc5ec8b7a4374ebcbecf6e7ad7c4d
Beludiiorio <beludiiorio@gmail.com>
2022-05-29 14:23:40 -0300

    cambios

--- area_rectangulo.php
+++ area_rectangulo.php
@@ -3,15 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-#Definición de la funcion: 
+$base= 100;
+$altura = 0.60;
 
-function calcularAreaRect($base, $altura) {
-    return $base * $altura;
+//Definicion:
+function calcularAreaRect ($base, $altura) {
+    return $base * $altura ;
 }
 
-# Uso de la misma:
+//Uso:
 
-echo "El área es " . calcularAreaRect(100, 0.60) . "<br>";
-echo "El área es " . calcularAreaRect(800, 300);
-
-?>
+echo "El area es" . calcularAreaRect(100,0.60)."<br>";
+echo "El area es" . calcularAreaRect(800,300);
