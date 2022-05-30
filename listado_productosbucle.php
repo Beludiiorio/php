e1c57b4b112138b9faafb029662432cd95bd308f
Beludiiorio <beludiiorio@gmail.com>
2022-05-30 11:04:55 -0300

    cambios

--- listado_productosbucle.php
+++ listado_productosbucle.php
@@ -3,82 +3,86 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-$aProductos = array();
-$aProductos[] = array(
+array();
+$aProductos[0] = array(
     "nombre" => "Smart TV 55\" 4K UHD",
     "marca" => "Hitachi",
     "modelo" => "554KS20",
     "stock" => 60,
     "precio" => 58000
 );
-$aProductos[] = array(
+$aProductos[1] = array(
     "nombre" => "Samsung Galaxy A30 Blanco",
     "marca" => "Samsung",
-    "modelo" => "Galaxy A30",
+    "modelo" => "Galaxy A30 ",
     "stock" => 0,
-    "precio" => 22000,
+    "precio" => 22000
 );
-$aProductos[] = array(
-    "nombre" => "Aire Acondicionado Split Inverter Frío/Calor Surrey 2900F",
-    "marca" => "Surrey",
+$aProductos[2] = array(
+    "nombre" => "Aire acondicionado Split F/C Surrey 2900F",
+    "marca" => " Surrey",
     "modelo" => "553AIQ1201E",
     "stock" => 5,
-    "precio" => 45000,
+    "precio" => 45000
 );
 
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listado de productos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Listado de productos </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <div class="container">
+<div class="container">
         <div class="row">
             <div class="col-12 text-center p-5">
                 <h1>Listado de productos</h1>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <table class="table table-hover border">
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Marca</th>
-                        <th>Modelo</th>
-                        <th>Stock</th>
-                        <th>Precio</th>
-                        <th>Acción</th>
-                    </tr>
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-hover border">
+                <tr>
+                    <th> Nombre </th>
+                    <th> Marca </th>
+                    <th>Modelo </th>
+                    <th> Stock </th>
+                    <th> Precio </th>
+                    <th> Acción</th>
+                </tr>
 
-                    <?php
-                    $contador = 0;
-                    while ($contador < count ($aProductos)){  //El count me devuelve la cantidad de elementos que tenga en mi array  
-                    ?>
-                    
-                        <tr>
-                            <td><?php echo $aProductos[$contador]["nombre"]; ?></td>
-                            <td><?php echo $aProductos[$contador]["marca"]; ?></td>
-                            <td><?php echo $aProductos[$contador]["modelo"]; ?></td>
-                            <td><?php echo $aProductos[$contador]["stock"] == 0 ? "No hay stock" : ($aProductos[0]["stock"] > 10 ? "Hay stock" : "Poco stock"); ?></td>
-                            <td>$ <?php echo $aProductos[$contador]["precio"]; ?></td>
-                            <td><button class="btn btn-primary">Comprar</button></td>
-                        </tr>
-                    <?php
-                        $contador++;
-                    }
-                    ?>
-                    
+                <?php 
+                
+                $contador= 0;
+                while ($contador < count ($aProductos)) {   //El count me devuelve la cantidad de elementos que tenga en mi array                  
+
+                ?>
+
+                <tr>
+                    <td> <?php echo $aProductos [$contador]["nombre"]?></td>
+                    <td> <?php echo $aProductos [$contador]["marca"] ?></td>
+                    <td> <?php echo $aProductos [$contador]["modelo"] ?></td>
+                    <td> <?php echo $aProductos [$contador]["stock"] == 0? "Sin stock" : ($aProductos [0]["stock"] <=10 ? "Poco stock" : "Hay stock" ) ?> </td>
+                    <td> $ <?php echo $aProductos [$contador]["precio"] ?></td>
+                    <td> <button class="btn btn-primary"> Comprar </button></td>
+                </tr>
+
+                <?php
+
+                $contador ++ ; 
+                }
+                ?> 
                 </table>
             </div>
         </div>
     </div>
 </body>
-
 </html>
