5338aaed89edfd85c7029aa2d7fdddd121a74dca
Beludiiorio <beludiiorio@gmail.com>
2022-05-30 13:37:31 -0300

    cambios

--- neto.php
+++ neto.php
@@ -1,16 +1,18 @@
-<?php
-ini_set('display_errors', 1);
+<?php //Abro php
+ini_set('display_errors', 1); //Nos ayuda a detectar los errores
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 //Definicion:
 
-function calcularNeto ($bruto) {
+function calcularNeto ($bruto) { // calcularNeto recibe a la variable $bruto
     return $bruto - ($bruto * 0.17);
 }
 
 //Uso:
 
-echo "El sueldo neto es $ " . calcularNeto(50000);
+echo "El sueldo neto es $ " . calcularNeto(50000); //Llamada a la funcion
+
+//50000 es el valor que va recibir la variable $bruto
 
 ?>
