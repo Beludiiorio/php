1a557225961432084b8b24dd7621645f70b91ce7
Beludiiorio <beludiiorio@gmail.com>
2022-05-30 17:07:36 -0300

    cambios

--- contar.php
+++ contar.php
@@ -1,31 +1,35 @@
-<?php
-ini_set('display_errors', 1);
+<?php //Abro php
+ini_set('display_errors', 1); //Las 3 lineas me muestran los errores que quedan almacenados en el apache
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$aPacientes = array();
-$aPacientes[] = array(
+
+$aNotas=  array(9,8,9.50,4,7,8); //Es un array de 6 elementos
+$aEmpleados = array("gomez","ramirez","di iorio","barrionuevo","gonzalez","perez","tucci");
+
+$aPacientes = array(); //Defino el array que es una lista de elementos
+$aPacientes[0] = array( //Hay un array asociativo con 4 elementos
     "dni" => "33.765.012",
     "nombre" => "Ana Acuña",
     "edad" => 45,
     "peso" => 82
 );
 
-$aPacientes[] = array(
+$aPacientes[1] = array( //El $aPacientes 0,1 y 2 son array numericos y cuando hay palabras es asociativo.
     "dni" => "23.684.385",
     "nombre" => "Gonzalo Bustamante",
     "edad" => 66,
     "peso" => 79,
 );
 
-$aPacientes[] = array(
+$aPacientes[2] = array(
     "dni" => "23.684.385",
     "nombre" => "Juan Irraola",
     "edad" => 28,
     "peso" => 90,
 );
 
-$aPacientes[] = array(
+$aPacientes[3] = array(
     "dni" => "36.630.478",
     "nombre" => "Beatriz Ocampo",
     "edad" => 50,
@@ -33,21 +37,19 @@ $aPacientes[] = array(
 
 );
 
-$aNotas=  array(9,8,9.50,4,7,8);
-$aEmpleados = array("gomez","ramirez","di iorio","barrionuevo","gonzalez","perez","tucci");
-
-
-
-function contar ($aArray){
-    $contador = 0;
-    foreach ($aArray as $item) {
-         $contador++;
+function contar ($aArray) { //La funcion contar recibe como parametro un array
+    $contador = 0; //$contador se almacena en memoria RAM
+    foreach ($aArray as $item) { //El foreach recorre cada elemento del array.
+   
+    $contador++; //$contador= $contador +1.
     }
-    return $contador;
 
+    return $contador; //Me devuelve $contador.
 }
 
+echo "Cantidad de notas es: ". contar($aNotas) . "<br>";
 echo "Cantidad de pacientes es: ". contar($aPacientes) . "<br>";
-echo "Cantidad de empleados es: ". contar($aEmpleados) . "<br>";
-echo "Cantidad de notas es: ". contar($aNotas);
+echo "Cantidad de empleados es: ". contar($aEmpleados);
+
+?>
 
