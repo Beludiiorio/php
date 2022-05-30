03296725ffefd683df64e9d66f89b8879ea885cd
Beludiiorio <beludiiorio@gmail.com>
2022-05-30 10:44:42 -0300

    cambios

--- funcion_promediar.php
+++ funcion_promediar.php
@@ -3,20 +3,23 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function promediar($aNumeros){
+
+
+//Definicion
+
+function promediar($aNumeros) {
     $suma = 0;
     foreach ($aNumeros as $numero) {
-        $suma = $suma + $numero;
-    }
-    $promedio = $suma / count($aNumeros);
-    return $promedio;
+$suma = $suma + $numero; //Es lo mismo que poner $suma += $numero; 
+ }
+$promedio= $suma / count($aNumeros);
+return $promedio;
 }
 
 
 //Uso
-$aNotas = array(8, 4, 5, 3, 9, 1);
-$aSueldo = array(800.30, 400.87, 500.45, 300, 900.70, 100, 900, 1800);
 
-echo "La nota promedio es: " . promediar($aNotas) ;
- 
+$aNotas= array(8,4,5,3,9,1);
+echo "El promedio es: " . promediar($aNotas). "<br>";
+
 ?>
