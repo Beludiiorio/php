ebe516c36a0e54defbec22d9f6834170e2968c06
Beludiiorio <beludiiorio@gmail.com>
2022-05-30 13:27:16 -0300

    cambios

--- area_rectangulo.php
+++ area_rectangulo.php
@@ -1,17 +1,18 @@
-<?php
-ini_set('display_errors', 1);
+<?php   //Abrimos php 
+ini_set('display_errors', 1);//Las primeras 3 lineas las colocamos para que xamp nos muestre los posibles errores de nuestro php 
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$base= 100;
+$base= 100; //Tomamos las variables de base y altura
 $altura = 0.60;
 
 //Definicion:
-function calcularAreaRect ($base, $altura) {
+function calcularAreaRect ($base, $altura) { //Llamada o invocacion a la funcion
     return $base * $altura ;
 }
 
-//Uso:
+//Uso:    
 
-echo "El area es" . calcularAreaRect(100,0.60)."<br>";
-echo "El area es" . calcularAreaRect(800,300);
+echo "El area es" . calcularAreaRect(100,0.60)."<br>"; //Utilizamos el echo para mostrar el resultado en pantalla
+echo "El area es" . calcularAreaRect(800,300);         //El br es un etiqueta de html para generar un salto de linea
+ 
